090f65c0b5c9c8a08605becfc2442f60d8f2e0f6
Joe <JoeG30@outlook.com>
2020-10-11 17:27:21 -0400

    More stuff

--- eFinance/src/Decision/DecisionController.java
+++ eFinance/src/Decision/DecisionController.java
@@ -5,8 +5,12 @@
  */
 package Decision;
 
+import Accounts.AccountsModel;
+import Application.AppModel;
+import Login.Main;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 
 /**
@@ -16,6 +20,10 @@ import javafx.fxml.Initializable;
  */
 public class DecisionController implements Initializable {
 
+    private Main application;
+    
+    private AppModel userApplication;
+    private AccountsModel currentUser;
     /**
      * Initializes the controller class.
      */
@@ -24,4 +32,22 @@ public class DecisionController implements Initializable {
         // TODO
     }    
     
+        //Gets the Application Object so this class can call the main method functions and get user information
+    public void setApp(Main application){
+        this.application = application;
+        currentUser = application.getLoggedUser();
+    }
+    
+    //Takes the user back to the log in page
+    public void logout(ActionEvent e){
+        application.userLogout();
+    }
+    
+    public void approveApplication(ActionEvent e){
+        userApplication.setApprovedApplication(true);
+    }
+    
+    public void denyApplication(ActionEvent e){
+        userApplication.setApprovedApplication(false);
+    }
 }
