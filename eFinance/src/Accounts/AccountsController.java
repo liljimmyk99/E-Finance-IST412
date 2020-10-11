498411a3808f2a3340746b11b5a86b41e47815c5
Alex <adc5541.psu.edu>
2020-10-11 17:19:33 -0400

    Updated Accounts Package

--- eFinance/src/Accounts/AccountsController.java
+++ eFinance/src/Accounts/AccountsController.java
@@ -6,10 +6,35 @@
 
 package Accounts;
 
+import Login.Main;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
 /**
  *
  * @author Alex Clapper
  */
-public class AccountsController {
+public class AccountsController implements Initializable {
+    
+    private Main application;
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
 
+    public void createAccount(String id, String email, String Status){
+        if(AccountsModel.getUsers().containsKey(id)){
+            System.out.println("A user with this id already exists");
+        }else{
+            AccountsModel.of(id).setEmail(email);
+            AccountsModel.of(id).setStatus(Status); 
+        }
+        
+    }    
+    
 }
