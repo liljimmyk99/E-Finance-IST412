471e19aa62c30b27cdbb46d17a18ecc881f707d9
Joe <JoeG30@outlook.com>
2020-10-11 16:50:30 -0400

    Added decision model

--- eFinance/src/Decision/DecisionModel.java
+++ eFinance/src/Decision/DecisionModel.java
@@ -5,10 +5,66 @@
  */
 package Decision;
 
+import java.util.HashMap;
+
 /**
  *
  * @author jimmykane
  */
 public class DecisionModel {
     
+    //Joe made these------
+    private Integer creditScore;
+    private boolean debtAproval;//If true, debt level has been approved and the application can be approved.
+    private String name;
+    
+    HashMap<String, String> questionsAndAnswers = new HashMap<String, String>();
+    
+    public DecisionModel(){
+        DecisionModel decisionModel = new DecisionModel();
+    }
+    
+       //Get application?????-----------------**********
+
+    /**
+     * @return the creditScore
+     */
+    public Integer getCreditScore() {
+        return creditScore;
+    }
+
+    /**
+     * @param creditScore the creditScore to set
+     */
+    public void setCreditScore(Integer creditScore) {
+        this.creditScore = creditScore;
+    }
+
+    /**
+     * @return the debtAproval
+     */
+    public boolean isDebtAproval() {
+        return debtAproval;
+    }
+
+    /**
+     * @param debtAproval the debtAproval to set
+     */
+    public void setDebtAproval(boolean debtAproval) {
+        this.debtAproval = debtAproval;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 }
