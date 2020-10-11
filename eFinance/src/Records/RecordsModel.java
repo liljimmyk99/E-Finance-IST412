b2ccb11c5dddf141d7c4b104a2a05d52868f4853
Jimmy Kane <liljimmykane@comcast.net>
2020-10-11 17:15:52 -0400

    Updated RecordsModel

--- eFinance/src/Records/RecordsModel.java
+++ eFinance/src/Records/RecordsModel.java
@@ -5,10 +5,41 @@
  */
 package Records;
 
+import Application.AppModel;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author jimmykane
  */
 public class RecordsModel {
+    private Map<String, Application.AppModel> apps = new HashMap<String, Application.AppModel>();
+    private Map<String, String> paymentHistory = new HashMap<String, String>();
+    
+    public RecordsModel(){
+        
+    }
+    
+    public RecordsModel(Map<String, Application.AppModel> apps, Map<String, String> paymentHistory){
+        this.apps = apps;
+        this.paymentHistory = paymentHistory;
+    }
+
+    public Map<String, AppModel> getApps() {
+        return apps;
+    }
+
+    public void setApps(Map<String, AppModel> apps) {
+        this.apps = apps;
+    }
+
+    public Map<String, String> getPaymentHistory() {
+        return paymentHistory;
+    }
+
+    public void setPaymentHistory(Map<String, String> paymentHistory) {
+        this.paymentHistory = paymentHistory;
+    }
     
 }
